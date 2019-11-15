d239b2fdb0e153e8c98daf4aea389504c0db5e6c
Vila do Site <admin@viladosite.com.br>
2019-11-15 00:41:48 -0300

    Inclusão do node no technologies

--- includes/sections/technologies.php
+++ includes/sections/technologies.php
@@ -3,28 +3,33 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-2 col-sm-6">
-          <a href="#">
+          <a href="https://www.w3.org/html/" target="_blank">
             <img class="img-fluid d-block mx-auto" src="img/logos/html.jpg" alt="">
           </a>
         </div>
         <div class="col-md-2 col-sm-6">
-          <a href="#">
+          <a href="https://www.w3.org/Style/CSS/" target="_blank">
             <img class="img-fluid d-block mx-auto" src="img/logos/css.jpg" alt="">
           </a>
         </div>
         <div class="col-md-2 col-sm-6">
-          <a href="#">
+          <a href="https://www.php.net/" target="_blank">
             <img class="img-fluid d-block mx-auto" src="img/logos/php7.jpg" alt="">
           </a>
         </div>
         <div class="col-md-2 col-sm-6">
-          <a href="#">
+          <a href="https://wordpress.org/" target="_blank">
             <img class="img-fluid d-block mx-auto" src="img/logos/wordpress.jpg" alt="">
           </a>
         </div>
         <div class="col-md-2 col-sm-6">
-          <a href="#">
+          <a href="https://www.mysql.com/" target="_blank">
             <img class="img-fluid d-block mx-auto" src="img/logos/mysql.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-6">
+          <a href="https://nodejs.org/" target="_blank">
+            <img class="img-fluid d-block mx-auto" src="img/logos/node.jpg" alt="">
           </a>
         </div>
       </div>
