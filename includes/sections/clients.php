b98c07b33d8fb2e7bf8b354059877efb5b7d6e05
Vila do Site <admin@viladosite.com.br>
2019-11-15 00:42:02 -0300

    Inclusão do clients

---
+++ includes/sections/clients.php
@@ -0,0 +1,102 @@
+<!-- Team -->
+<section class="bg-light page-section" id="team">
+
+  <div class="container">
+
+
+    <div class="row">
+
+      <div class="col-lg-12 text-center">
+        <h2 class="section-heading text-uppercase">Nossos Clientes</h2>
+        <h3 class="section-subheading text-muted">Veja o que dizem alguns de nossos clientes sobre o nosso trabalho:</h3>
+      </div>
+
+    </div>
+    
+
+    <div class="row">
+      
+      <div class="col-sm-3">
+        <div class="team-member">
+          <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
+          <h4>Gustavo Sotero</h4>
+          <p class="text-muted">Jardim Digital</p>
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="http://jardimdigital.com.br" target="_blank">
+                <i class="fas fa-globe-americas"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/jardim.in.3/" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="team-member">
+          <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
+          <h4>Mayra Jucá</h4>
+          <p class="text-muted">Espalhafato Comunicação</p>
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="http://espalhafato.com.br" target="_blank">
+                <i class="fas fa-globe-americas"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="team-member">
+          <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+          <h4>Cecília Schmidt</h4>
+          <p class="text-muted">Breves e Schmidt</p>
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="http://breveseschmidt.com.br" target="_blank">
+                <i class="fas fa-globe-americas"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="team-member">
+          <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+          <h4>Viviane Logullo</h4>
+          <p class="text-muted">Anagea</p>
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="http://anagea.org.br" target="_blank">
+                <i class="fas fa-globe-americas"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/anagea.rj/" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="row">
+      
+      <div class="col-lg-8 mx-auto text-center">
+        <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+      </div>
+
+    </div>
+
+
+  </div>
+</section>
