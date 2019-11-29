48be4a083a42bdbd78d02fc96f366065855e19b2
bruckbmrj <bruckbmrj@hotmail.com>
2019-11-29 12:40:16 -0300

    alterar email de destino e headers

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -29,17 +29,19 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+
+$mailto = "mailto:$email_address?subject= $email_subject";
+$tel = "tel:$phone";
    
 // Create the email and send the message
-$to = 'dev@viladosite.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'admin@viladosite.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "$name entrou em contato pelo site viladosite.com.br";
 $email_body = "Você recebeu uma nova mensagem do formulário de contato do site.<br>
 	<strong>Detalhes da mensagem:</strong><br>
 	<strong>Nome:</strong> $name <br>
-	<strong>Email:</strong> <a href='mailto:$email_address?subject= $email_subject'> <br>
-	<strong>Telefone:<strong> $phone <br>
-	<strango>Mensagem:<strong><br> $message";
-	
+	<strong>Email:</strong> <a href=".$mailto.">$email_address</a><br>
+	<strong>Telefone:</strong> <a href=".$tel.">$phone</a> <br>
+	<strong>Mensagem:</strong><br> $message";
 $headers = "From: noreply@viladosite.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address\n";
 $headers .= "Content-Type: text/html; charset=utf-8\n"; 
