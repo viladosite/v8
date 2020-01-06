42c373b7c92e63b7c2e5b506c1a510f07eb61d9e
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2020-01-06 18:21:21 -0300

    add digicampus modal

--- includes/modal/modals.php
+++ includes/modal/modals.php
@@ -484,7 +484,7 @@
     </div>
   </div>
 
-  <!-- Modal 11 -->
+  <!-- Modal 12 -->
   <div class="portfolio-modal modal fade" id="portfolioModal12" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -511,6 +511,46 @@
                 <ul class="list-inline">
                   <li><a href="http://camposmaia.com/v1/" target="_blank">camposmaia.com</a></li>
                   <li>Cliente: Campos Maia</li>
+                  <li>Serviço: Criação de Website</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                  <i class="fas fa-times"></i>
+                  Fechar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal 13 -->
+  <div class="portfolio-modal modal fade" id="portfolioModal13" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <!-- Project Details Go Here -->
+                <h2 class="text-uppercase">Digicampus</h2>
+                <p class="item-intro text-muted">Criação de Website</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/campos-maia-full.jpg" alt="">
+                <p>A Digicampus é uma iniciativa criada em conjunto pelos advogados Cristiana Maia, Vinicius Ferreira e pelo webmaster Diogo Fagundes para criar e disponibilizar para comercialização conteúdos didáticos sobre direito e internet.</p>
+                <p>
+                  O site digicampus.com.br se propõe a ser um campus virtual trazendo sistemas de integração com ambientes de aulas virtuais e conteúdos proprietários sobre direito e tecnologia.
+                </p>
+
+                <p>A Vila do Site participou ativamente na criação da identidade visual, logo e principalmente no layout e nos sistemas da digicampus.com.br que conta com área para downloads tanto pública quanto privada para os alunos e integrações com sistemas de EAD como o teachable.com e hotmart.com.</p>
+
+                <ul class="list-inline">
+                  <li><a href="https://digicampus.com.br/v1/" target="_blank">digicampus.com.br</a></li>
+                  <li>Cliente: Digicampus</li>
                   <li>Serviço: Criação de Website</li>
                 </ul>
                 <button class="btn btn-primary" data-dismiss="modal" type="button">
--- includes/sections/portfolio.php
+++ includes/sections/portfolio.php
@@ -10,6 +10,21 @@
       <div class="row">
 
       	<div class="col-md-4 col-sm-6 portfolio-item">
+      	  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal13">
+      	    <div class="portfolio-hover">
+      	      <div class="portfolio-hover-content">
+      	        <i class="fas fa-plus fa-3x"></i>
+      	      </div>
+      	    </div>
+      	    <img class="img-fluid" src="img/portfolio/digicampus-thumbnail.jpg" alt="">
+      	  </a>
+      	  <div class="portfolio-caption">
+      	    <h4>Digicampus</h4>
+      	    <p class="text-muted">Criação de Website</p>
+      	  </div>
+      	</div>
+
+      	<div class="col-md-4 col-sm-6 portfolio-item">
       	  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal12">
       	    <div class="portfolio-hover">
       	      <div class="portfolio-hover-content">
