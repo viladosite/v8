45ae46ed65f3f187bdd76c80da5631d38328c392
Bruno Braga Mendonça <bruckbmrj@hotmail.com>
2020-01-07 12:29:50 -0300

    add portal clube modal

--- includes/sections/portfolio.php
+++ includes/sections/portfolio.php
@@ -10,6 +10,21 @@
       <div class="row">
 
       	<div class="col-md-4 col-sm-6 portfolio-item">
+      	  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal15">
+      	    <div class="portfolio-hover">
+      	      <div class="portfolio-hover-content">
+      	        <i class="fas fa-plus fa-3x"></i>
+      	      </div>
+      	    </div>
+      	    <img class="img-fluid" src="img/portfolio/portal-clube-thumbnail.jpg" alt="">
+      	  </a>
+      	  <div class="portfolio-caption">
+      	    <h4>Portal Clube de Engenharia</h4>
+      	    <p class="text-muted">Criação de Website</p>
+      	  </div>
+      	</div>
+
+      	<div class="col-md-4 col-sm-6 portfolio-item">
       	  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal14">
       	    <div class="portfolio-hover">
       	      <div class="portfolio-hover-content">
